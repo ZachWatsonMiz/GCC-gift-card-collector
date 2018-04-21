373cf45c8c87a48c7bdbfd1847425974d5428b74
Zach Watson <35540447+ZachWatsonMiz@users.noreply.github.com>
2018-04-21 16:13:45 -0500

    Added Read Statements for All cards and location

--- utilities/SQL_statements.php
+++ utilities/SQL_statements.php
@@ -1,3 +1,14 @@
 <?php
     // Lists of SQL statements for CRUD operations
+
+//read all card data for specific user and sort by latest updated card
+SELECT location, balance, serial, dateAdded, balanceUpdate, expiration 
+FROM CertifiCards
+WHERE owner = $userId 
+ORDER BY balanceUpdate DESC
+
+//read transaction data for specific card by location
+SELECT Transaction.balanceDelta, Transactions.date 
+FROM Transactions, Certificards
+WHERE Certificards.id = Transactions.cardId && CertifiCards.location = $cardLocation
 ?>
