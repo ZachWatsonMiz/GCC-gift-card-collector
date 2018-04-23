110486133508f8f57f1215db39d55c4655d7f33d
Zach Watson <35540447+ZachWatsonMiz@users.noreply.github.com>
2018-04-23 13:25:28 -0500

    updated SQL statements

--- utilities/SQL_statements.php
+++ utilities/SQL_statements.php
@@ -2,14 +2,16 @@
     // Lists of SQL statements for CRUD operations
 
 //read all card data for specific user and sort by latest updated card
-SELECT location, balance, serial, dateAdded, balanceUpdate, expiration 
-FROM CertifiCards
-WHERE owner = $userId 
-ORDER BY balanceUpdate DESC
+SELECT location, balance, serial, dateAdded, updated, expiration 
+FROM certificards
+WHERE owner = $userID
+ORDER BY updated DESC
 
-//read transaction data for specific card by location
-SELECT Transaction.balanceDelta, Transactions.date 
-FROM Transactions, Certificards
-WHERE Certificards.id = Transactions.cardId && CertifiCards.location = $cardLocation
-ORDER BY Transactions.date DESC
+//read transaction data for specific card by card id
+SELECT transactions.balanceDelta, transactions.date 
+FROM transactions, certificards
+WHERE certificards.id = transactions.cardId && certificards.id = $cardID
+ORDER BY transactions.date DESC
+    
+
 ?>
